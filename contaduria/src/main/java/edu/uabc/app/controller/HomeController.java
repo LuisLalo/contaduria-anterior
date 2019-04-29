91c9682c4c4e479a32d13c9a1f1fa4e9fb9c42c1
adan_ <Adan.gama@uabc.edu.mc>
2019-04-29 12:30:49 -0700

    Ventanda comodin, campo descripcion en la BD

--- contaduria/src/main/java/edu/uabc/app/controller/HomeController.java
+++ contaduria/src/main/java/edu/uabc/app/controller/HomeController.java
@@ -1,11 +1,27 @@
 package edu.uabc.app.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import edu.uabc.app.model.Documento;
+import edu.uabc.app.model.Ventana;
+import edu.uabc.app.service.IDocumentoService;
+import edu.uabc.app.service.IVentanaService;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private IDocumentoService documentoService;
+	
+	@Autowired
+	private IVentanaService ventanaService;
 
 	@RequestMapping(value="/login", method=RequestMethod.GET)
 	public String mostrarLogin() {
@@ -16,4 +32,18 @@ public class HomeController {
 	public String mostrarPrincipal() {
 		return "home";
 	}
+	
+	@GetMapping(value = "{id}")
+	public String mostrarVentana(@RequestParam int idVentana, Model modelo) {
+		
+		
+		
+		List<Documento> listadocs = documentoService.buscarPorIdVentanaOrdenPorOrden(idVentana);
+		Ventana ventana = ventanaService.buscarPorId(idVentana);
+		
+	modelo.addAttribute("ventana",ventana);
+	modelo.addAttribute("documentos",listadocs);
+		return "oficios";
+	}
+	
 }
